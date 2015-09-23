7eb3c66bee3f6ce1687f412c33fa480c6f14566e
Flor Kaucic <florkaucic@gmail.com>
2015-09-23 19:37:24 -0300

    Fusion

--- src/metodos/Fusion.java
+++ src/metodos/Fusion.java
@@ -1,11 +1,43 @@
 package metodos;
 
 public class Fusion {
-	public static <T extends Comparable<T>> T[] ordenarPorFusion(T[] vec){
-		T[] nuevo = vec;
-		//Arrays.copyOfRange(Object[] src, int from, int to)
+	public static <T extends Comparable<T>> T[] ordenarPorFusion(T[] vec) {
+		return fusion(vec, 0, vec.length, 0);
+	}
+
+	private static <T extends Comparable<T>> T[] fusion(T[] vec, int ini, int fin, int p) {
+		int dim = fin - ini;
 		
-	    
-		return nuevo;
+		if (dim > 1) {
+			int mitad = ini + (dim / 2);
+
+			fusion(vec, ini, mitad, p + 1);
+			fusion(vec, mitad, fin, p + 1);
+
+			@SuppressWarnings("unchecked")
+			T[] aux = (T[]) (new Comparable[dim]);
+			
+			for (int a = 0; a < (mitad - ini); a++)
+				aux[a] = vec[a + ini];
+
+			for (int b = 0; b < (fin - mitad); b++)
+				aux[b+dim/2] = vec[fin - (b + 1)];
+
+			int i = 0, j = aux.length-1, k = ini;
+
+			while (i < j) {
+				if (aux[i].compareTo(aux[j]) < 0) {
+					vec[k] = aux[i];
+					i++;
+				} else {
+					vec[k] = aux[j];
+					j--;
+				}
+				k++;
+			}
+			vec[k] = aux[i];
+		}
+
+		return vec;
 	}
 }
--- src/tests/TestFusion.java
+++ src/tests/TestFusion.java
@@ -5,11 +5,11 @@ import metodos.Fusion;
 public class TestFusion {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Integer[] vector = { 1, 2, 3 };
-		Integer[] nuevo = Fusion.ordenarPorFusion(vector);
-		for (int i = 0; i < nuevo.length; i++) 
-			System.out.println(i+": "+nuevo[i]);
+		// TODO Auto-generated method stub}				
+		Integer[] enteros = { 3, 1, 4, 2, 8, 0, 5, 7, 6, 9, 5, 3};
+		enteros = Fusion.ordenarPorFusion(enteros);
+		for (int i = 0; i < enteros.length; i++)
+			System.out.println(i + ": " + enteros[i]);	
 	}
 
 }
