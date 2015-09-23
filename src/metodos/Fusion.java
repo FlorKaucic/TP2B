535c4eb5298aded3a447d0054218481944a0531b
Flor Kaucic <florkaucic@gmail.com>
2015-09-23 14:06:26 -0300

    Ejemplo de estructura en Fusion

--- src/metodos/Fusion.java
+++ src/metodos/Fusion.java
@@ -1,7 +1,10 @@
 package metodos;
 
 public class Fusion {
-	public static void ordenarPorFusion(){
+	public static <T extends Comparable<T>> T[] ordenarPorFusion(T vec[]){
+		T[] nuevo = null;
 		
+		
+		return nuevo;
 	}
 }
