6e30968c1d0f79ef0782b7afddca748de667769d
pdiego93 <pdiego93@gmail.com>
2015-09-23 19:05:32 -0300

    Generador en TestShell

--- src/metodos/Burbujeo.java
+++ src/metodos/Burbujeo.java
@@ -1,7 +1,18 @@
 package metodos;
 
 public class Burbujeo {
-	public static void ordenarPorBurbujeo() {
+	public static < O extends Comparable <O>> void ordenarPorBurbujeo(O [] vec) {
+		O aux;
+		for (int i = 0; i < vec.length; i++) {
+
+			for (int j = 0; j < vec.length-1; j++) {
+				if (vec[j].compareTo(vec[j + 1]) > 0) {
+					aux = vec[j];
+					vec[j] = vec[j + 1];
+					vec[j + 1] = aux;
+				}
+			}
 
 	}
+}
 }
--- src/tests/TestShell.java
+++ src/tests/TestShell.java
@@ -1,21 +1,40 @@
 package tests;
 import metodos.Shell;
 
+import java.util.Random;
+
+import auxiliar.Complejo;
+
 public class TestShell {
 
 	public static void main(String[] args) {
-		Integer [] v = new Integer[6];
-		v[0]=5;
-		v[1]=6;
-		v[2]=9;
-		v[3]=3;
-		v[4]=1;
-		v[5]=4;
+		int cant = 1000;
+		//Integer [] v = new Integer[6];
+		Complejo [] c = new Complejo[cant];
+		Random randomDouble = new Random();
+				
+		/*for (int i = 0; i < cant; i++){
+			c[i]=new Complejo(randomDouble.nextDouble() * 20 - 10, randomDouble.nextDouble() * 20 - 10);
+			System.out.println(c[i]);
+		}*/
+		/*for (int i = 0; i < cant; i++){
+			c[i]=new Complejo(i, i);
+			System.out.println(c[i]);
+		}*/
+		for (int i = 0; i < cant; i++){
+			c[i]=new Complejo(cant-i, cant-i);
+			System.out.println(c[i]);
+		}
+		long tIni = System.currentTimeMillis();
 		
-		Shell.ordenarPorShell(v);
-		for (Integer i :v)
-			System.out.println(i);
+		Shell.ordenarPorShell(c);
 		
+		long tFin = System.currentTimeMillis();
+		
+		System.out.println("Vector ordenado:");
+		for (Complejo com :c)
+			System.out.println(com);
+		System.out.println("Tiempo de resolucion para el vector: " + (tFin - tIni));
 	}
 
 }
