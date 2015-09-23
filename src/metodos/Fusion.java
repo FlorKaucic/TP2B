51cb1cd2ecf45d7b58f42c164202759bb65fb9db
Flor Kaucic <florkaucic@gmail.com>
2015-09-23 14:08:37 -0300

    Ejemplo de estructura en Fusion Ignorar los commits anteriores

--- src/metodos/Fusion.java
+++ src/metodos/Fusion.java
@@ -1,14 +1,8 @@
 package metodos;
 
 public class Fusion {
-<<<<<<< HEAD
-	public static <T extends Comparable<T>> T[] ordenarPorFusion(T vec[]){
+	public static <T extends Comparable<T>> T[] ordenarPorFusion(T[] vec){
 		T[] nuevo = null;
-=======
-	public static <T extends Comparable<T>> T ordenarPorFusion(T vec[]){
-		T nuevo = null;
->>>>>>> refs/remotes/origin/master
-		
 		
 		return nuevo;
 	}
