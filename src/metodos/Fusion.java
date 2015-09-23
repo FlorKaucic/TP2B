11db1d41dae6cf7d0a0afaa1889253d091416e37
Flor Kaucic <florkaucic@gmail.com>
2015-09-23 14:36:36 -0300

    Paquete auxiliar

--- src/metodos/Fusion.java
+++ src/metodos/Fusion.java
@@ -2,8 +2,8 @@ package metodos;
 
 public class Fusion {
 	public static <T extends Comparable<T>> T[] ordenarPorFusion(T[] vec){
-		T[] nuevo = null;
-		
+		T[] nuevo = vec;
+		//Arrays.copyOfRange(Object[] src, int from, int to)
 		return nuevo;
 	}
 }
