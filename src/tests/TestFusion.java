c883256fc1601634f5259e777720ba9c83df2707
Flor Kaucic <florkaucic@gmail.com>
2015-09-23 19:57:06 -0300

    Fusion, ahora hago el main y la doc

--- src/tests/TestFusion.java
+++ src/tests/TestFusion.java
@@ -1,5 +1,8 @@
 package tests;
 
+import java.util.Random;
+
+import auxiliar.Complejo;
 import metodos.Fusion;
 
 public class TestFusion {
@@ -10,6 +13,47 @@ public class TestFusion {
 		enteros = Fusion.ordenarPorFusion(enteros);
 		for (int i = 0; i < enteros.length; i++)
 			System.out.println(i + ": " + enteros[i]);	
+		
+		int cant = 1500;
+		
+		
+		Complejo [] c = new Complejo[cant];
+		Random randomDouble = new Random();
+				
+		System.out.println("Complejos random");
+		for (int i = 0; i < cant; i++){
+			c[i]=new Complejo(randomDouble.nextDouble() * 20 - 10, randomDouble.nextDouble() * 20 - 10);
+			System.out.println(c[i] + " Modulo:"+c[i].modulo());
+		}
+		c = Fusion.ordenarPorFusion(c);
+		System.out.println("Ordenados");
+		for (int i = 0; i < c.length; i++)
+			System.out.println(i + ": " + c[i] + " Modulo:"+c[i].modulo());	
+		
+		
+		
+		System.out.println("Complejos i i");
+		for (int i = 0; i < cant; i++){
+			c[i]=new Complejo(i, i);
+			System.out.println(c[i] + " Modulo:"+c[i].modulo());
+		}
+		c = Fusion.ordenarPorFusion(c);
+		System.out.println("Ordenados");
+		for (int i = 0; i < c.length; i++)
+			System.out.println(i + ": " + c[i] + " Modulo:"+c[i].modulo());	
+		
+		
+
+		System.out.println("Complejos i i al reves");
+		for (int i = 0; i < cant; i++){
+			c[i]=new Complejo(cant-i, cant-i);
+			System.out.println(c[i] + " Modulo:"+c[i].modulo());
+		}
+		c = Fusion.ordenarPorFusion(c);
+		System.out.println("Ordenados");
+		for (int i = 0; i < c.length; i++)
+			System.out.println(i + ": " + c[i] + " Modulo:"+c[i].modulo());	
+		
 	}
 
 }
