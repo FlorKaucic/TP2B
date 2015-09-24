eabfd347f8ce32d5fb65be12b7259bb42e14e01d
pdiego93 <pdiego93@gmail.com>
2015-09-23 21:07:51 -0300

    Nada 2

--- src/tests/TestBurbujeo.java
+++ src/tests/TestBurbujeo.java
@@ -9,20 +9,20 @@ public class TestBurbujeo {
 
 	public static void main(String[] args) {
 
-		int cant = 5;
+		int cant = 1000;
 		//Integer [] v = new Integer[6];
 		Complejo [] c = new Complejo[cant];
 		Random randomDouble = new Random();
-				
-		/*for (int i = 0; i < cant; i++){
+		/*		
+		for (int i = 0; i < cant; i++){
 			c[i]=new Complejo(randomDouble.nextDouble() * 20 - 10, randomDouble.nextDouble() * 20 - 10);
 			System.out.println(c[i]);
 		}*/
 		/*for (int i = 0; i < cant; i++){
 			c[i]=new Complejo(i, i);
 			System.out.println(c[i]);
-		}*/
-		for (int i = 0; i < cant; i++){
+		}
+		*/for (int i = 0; i < cant; i++){
 			c[i]=new Complejo(cant-i, cant-i);
 			System.out.println(c[i]);
 		}
--- src/tests/TestFusion.java
+++ src/tests/TestFusion.java
@@ -7,7 +7,12 @@ public class TestFusion {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer[] vector = { 1, 2, 3 };
+		
+		
 		Integer[] nuevo = Fusion.ordenarPorFusion(vector);
+		
+		
+		
 		for (int i = 0; i < nuevo.length; i++) 
 			System.out.println(i+": "+nuevo[i]);
 	}
