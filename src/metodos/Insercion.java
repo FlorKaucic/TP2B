1b45adbd2b7551e9f5ee800385fdb03a493d2633
JJProgram <juanjopalopoli@hotmail.com>
2015-09-24 18:12:19 +0000

    Con Centinela. Falta analizar por que fallan ambos.

--- src/metodos/Insercion.java
+++ src/metodos/Insercion.java
@@ -4,8 +4,30 @@ public class Insercion {
 	public static < I extends Comparable <I>> void ordenarPorInsercionSinCentinela(I [] vec)  {
 		I aux;
 		int j;
-		int i;
-		for (i = 1; i < vec.length; i++)
+		for (int i = 1; i < vec.length; i++)
+		{
+			aux = vec[i];
+			j = i - 1;
+			while ( (vec[j].compareTo(aux)==1) && (j >= 0) )
+			{
+				vec[j + 1] = vec[j];
+				j--;
+			}
+				vec[j + 1] = aux;
+		}	
+	}
+	public static <I extends Comparable <I>> void ordenarPorInsercionConCentinela(I[] vec) {
+		I aux;
+		int j;
+		for (j = 0; j < vec.length-1; j++) {
+			if (vec[j].compareTo(vec[j + 1]) > 0) {
+				aux = vec[j];
+				vec[j] = vec[j + 1];
+				vec[j + 1] = aux;
+			}
+		}
+		j = 0;
+		for (int i = 1; i < vec.length; i++)
 		{
 			aux = vec[i];
 			j = i - 1;
