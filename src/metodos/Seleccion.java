ab948ea2dafca16223aaf8e2d06723a66c4664ae
Flor Kaucic <florkaucic@gmail.com>
2015-09-23 22:30:12 -0300

    Con comentarios

--- src/metodos/Seleccion.java
+++ src/metodos/Seleccion.java
@@ -2,17 +2,20 @@ package metodos;
 
 public class Seleccion {
 	public static <T extends Comparable<T>> void ordenarPorSeleccion(T[] vec) {
-		int minIndex;
+		int mini;
 		T aux;
 		for (int i = 0; i < vec.length - 1; i++) {
-			minIndex = i;
+			// Establezco que el mas chico es el primero de este ciclo, guardo el indice
+			mini = i;
+			// Lo comparo con todos, si uno es mas chico, guardo el indice
 			for (int j = i + 1; j < vec.length; j++)
-				if (vec[j].compareTo(vec[minIndex])<0)
-					minIndex = j;
-			if (minIndex != i) {
+				if (vec[j].compareTo(vec[mini])<0)
+					mini = j;
+			// Si el indice es diferente al primero, los intercambio
+			if (mini != i) {
 				aux = vec[i];
-				vec[i] = vec[minIndex];
-				vec[minIndex] = aux;
+				vec[i] = vec[mini];
+				vec[mini] = aux;
 			}
 		}
 	}
